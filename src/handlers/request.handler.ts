383270aba6e83bedcfb44dd8b6ea11eb2cd1da45
francis.kinuthia <francis.kinuthia@softwaregroup.com>
2023-02-21 15:57:07 +0300

    fix broken tests

--- src/handlers/request.handler.ts
+++ src/handlers/request.handler.ts
@@ -1,10 +1,10 @@
-import {Readable, Transform} from 'stream';
-import {pipeline} from 'stream/promises';
+import * as s from 'stream';
+import * as sp from 'stream/promises';
 import CoreRequestHandler from '../core/core.request.handler';
 import {validateRequest} from '../middleware/core.validate.request';
 import {dbProcessRequest} from '../middleware/db.process.request';
 
-const RequestHandler = async (dataStream: Readable): Promise<any> => {
+const RequestHandler = async (dataStream: s.Readable): Promise<any> => {
   const preRequestHandler = new CoreRequestHandler();
   preRequestHandler.registerMiddleware(validateRequest);
 
@@ -14,7 +14,7 @@ const RequestHandler = async (dataStream: Readable): Promise<any> => {
   const postRequestHandler = new CoreRequestHandler();
   postRequestHandler.registerMiddleware(validateRequest);
 
-  return pipeline<Readable, Transform, Transform, Transform>(
+  return sp.pipeline<s.Readable, s.Transform, s.Transform, s.Transform>(
     dataStream,
     preRequestHandler,
     requestHandler,
